46642ae4b2e705e36b88003fb3fd6bf039466e96
John Carlou Mangabo (Accenture International Limite) <john.c.mangab0@gmail.com>
2025-04-20 22:03:03 +0800

    Update title

--- views/header.php
+++ views/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Record System</title>
+    <title>School Record System</title>
     <link rel="stylesheet" href="../assets/styles/index.css">
     <link rel="stylesheet" href="assets/styles/index.css">
     <!-- Font Awesome CDN (Free) -->
